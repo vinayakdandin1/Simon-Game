d663de8c51b26ab0980dc4e00d231389dbebdd3f
vinayakdandin1 <vinayakdandin1@gmail.com>
2020-07-09 14:50:49 +0200

    Finished Simon Game with start button

---
+++ game.js
@@ -0,0 +1,84 @@
+let gamePattern = [];
+
+let buttonColours = ['red', 'blue', 'green', 'yellow'];
+
+let userClickedPattern = [];
+
+let started = false;
+
+let level = 0;
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+
+  $('#level-title').text('Level ' + level);
+
+  let randomNumber = Math.floor(Math.random() * 4);
+
+  let randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $('#' + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+$('.btn').click(function () {
+  var userChosenColour = $(this).attr('id');
+
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function playSound(name) {
+  var audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $('#' + currentColour).addClass('pressed');
+
+  setTimeout(function () {
+    $('#' + currentColour).removeClass('pressed');
+  }, 100);
+}
+
+$('.start').click(function () {
+  if (!started) {
+    $('#level-title').text('Level ' + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound('wrong');
+
+    $('body').addClass('game-over');
+
+    setTimeout(function () {
+      $('body').removeClass('game-over');
+    }, 200);
+
+    $('#level-title').text('Game Over, Press Start button to Restart');
+
+    level = 0;
+    gamePattern = [];
+    started = false;
+  }
+}
